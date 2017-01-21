a9954700f7c58dbff9d80e818127dc296e0b87d9
Laurens Mathot <laurens.mathot@codeanimo.com>
2017-01-21 11:04:27 +0100

    Touching people makes them angrier

--- Assets/Script/Mood.cs
+++ Assets/Script/Mood.cs
@@ -2,14 +2,39 @@
 
 public class Mood : MonoBehaviour {
     public float angriness = 0.0f;
+    public float angrinessPerFixedUpdate = 0.01f;
 
     public Collider playerCollider;
+
+    protected bool hittingPlayer = false;
+
+    private void Start()
+    {
+        angriness = 0.0f;
+        hittingPlayer = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (hittingPlayer)
+        {
+            angriness += angrinessPerFixedUpdate;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other == playerCollider)
         {
-            angriness += 0.1f;
+            hittingPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other == playerCollider)
+        {
+            hittingPlayer = false;
         }
     }
 }
