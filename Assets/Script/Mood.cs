2a89d446bd672021a4bc5f885cd85dded9e2b580
Laurens Mathot <laurens.mathot@codeanimo.com>
2017-01-21 10:41:27 +0100

    Angriness now slightly increases when a person hits the player

---
+++ Assets/Script/Mood.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Mood : MonoBehaviour {
+    public float angriness = 0.0f;
+
+    public Collider playerCollider;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == playerCollider)
+        {
+            angriness += 0.1f;
+        }
+    }
+}
