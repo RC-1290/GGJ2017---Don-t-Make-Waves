8c9d57bf2cab8d0a1b6232cb7c678ac45a64134a
Brian Davis <briandavism@gmail.com>
2017-01-22 00:31:22 +0100

    (Laurens commit) Falcon Punch

--- Assets/Script/Punch.cs
+++ Assets/Script/Punch.cs
@@ -7,6 +7,9 @@ public class Punch : MonoBehaviour
 
 	public float strength = 1.0f;
 
+    public float falconPunchModifier = 10.0f;
+    public string falconPunchAxis;
+
 	private void Start()
 	{
 		lastPos = transform.position;
@@ -21,12 +24,13 @@ public class Punch : MonoBehaviour
 
 	private void OnCollisionEnter(Collision collision)
 	{
+        float multiplier = falconPunchModifier * Input.GetAxis(falconPunchAxis);
 		PushOver target = collision.gameObject.GetComponent<PushOver>();
 		if (target)
 		{
 			target.TurnToPhysics();
 			Rigidbody targetRB = collision.gameObject.GetComponent<Rigidbody>();
-			targetRB.AddForceAtPosition(velocity * strength, collision.contacts[0].point);
+			targetRB.AddForceAtPosition(velocity * strength * multiplier, collision.contacts[0].point);
 		}
 	}
 }
