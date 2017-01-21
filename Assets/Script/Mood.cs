f6d9e556a8b049339d1687df1fd3f0ab3e93abec
Laurens Mathot <laurens.mathot@codeanimo.com>
2017-01-21 12:07:14 +0100

    People turn red (or a different color) based on how angry they are. Enabled VR support. Re-arranged scene to make more sense for VR

--- Assets/Script/Mood.cs
+++ Assets/Script/Mood.cs
@@ -5,13 +5,19 @@ public class Mood : MonoBehaviour {
     public float angrinessPerFixedUpdate = 0.01f;
 
     public Collider playerCollider;
+    public Color startingColor;
+    public Color angryColor;
 
+    protected Material moodColorMat;
+    protected int materialMoodId = -1;
     protected bool hittingPlayer = false;
 
     private void Start()
     {
         angriness = 0.0f;
         hittingPlayer = false;
+        moodColorMat = GetComponent<MeshRenderer>().material;
+        materialMoodId = Shader.PropertyToID("_Mood");
     }
 
     private void FixedUpdate()
@@ -19,6 +25,7 @@ public class Mood : MonoBehaviour {
         if (hittingPlayer)
         {
             angriness += angrinessPerFixedUpdate;
+            moodColorMat.SetFloat(materialMoodId, angriness);
         }
     }
 
