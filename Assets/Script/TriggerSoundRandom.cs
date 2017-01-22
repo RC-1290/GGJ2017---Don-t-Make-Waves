e69a1e5e3a24618a770138d2035492e64c8e1ff9
Laurens Mathot <laurens.mathot@codeanimo.com>
2017-01-22 16:06:05 +0100

    Added the meta files. Added ambient sounds teammember found. Triggering train sounds at random.

---
+++ Assets/Script/TriggerSoundRandom.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSoundRandom : MonoBehaviour {
+
+	public float minWait = 10.0f;
+	public float maxWait = 60.0f;
+
+	protected AudioSource source;
+	protected bool waiting = false;
+	protected float resumeTime = 0.0f;
+
+
+	private void Start()
+	{
+		source = GetComponent<AudioSource>();
+	}
+
+
+	private void Update()
+	{
+		if (!source.isPlaying)
+		{
+			if (waiting)
+			{
+				if (Time.timeSinceLevelLoad >= resumeTime)
+				{
+					source.Play();
+				}
+			}
+			else
+				{
+				resumeTime = Time.timeSinceLevelLoad + Random.Range(minWait, maxWait);
+				waiting = true;
+			}
+		}
+	}
+}
