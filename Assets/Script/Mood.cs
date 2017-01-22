c0b4d756066495403efe45ff5ce70ced65e6d9fa
Laurens Mathot <laurens.mathot@codeanimo.com>
2017-01-22 15:44:00 +0100

    Head pops and falls on the floor when looking at them for too long.

--- Assets/Script/Mood.cs
+++ Assets/Script/Mood.cs
@@ -9,10 +9,12 @@ public class Mood : MonoBehaviour {
 	public float maxHeadDist = 0.012f;
 
 	public Transform head;
+	public AudioSource headPopSound;
 
     public GameDirector director;
 
 	protected Vector3 neutralHeadPos;
+	protected Vector3 neutralHeadscale;
 
     protected bool hittingPlayer = false;
 
@@ -20,6 +22,7 @@ public class Mood : MonoBehaviour {
     {
         angriness = 0.0f;
 		neutralHeadPos = head.localPosition;
+		neutralHeadscale = head.localScale;
         hittingPlayer = false;
     }
 
@@ -35,18 +38,33 @@ public class Mood : MonoBehaviour {
 			angriness = angriness < 0.0f ? 0.0f : angriness;
 		}
 
-		// Mood based head jitter: 
-		float distanceMultiplier = headDistMultiplier * angriness;
-		Vector3 randomDir = new Vector3(
-			Mathf.Clamp( Random.Range(-distanceMultiplier, distanceMultiplier), -maxHeadDist, maxHeadDist),
-			Mathf.Clamp( Random.Range(-distanceMultiplier, distanceMultiplier), -maxHeadDist, maxHeadDist),
-			Mathf.Clamp( Random.Range(-distanceMultiplier, distanceMultiplier), -maxHeadDist, maxHeadDist));
-		head.localPosition = neutralHeadPos + randomDir;
+		if (head)
+		{
+			// Mood based head jitter: 
+			float distanceMultiplier = headDistMultiplier * angriness;
+			Vector3 randomDir = new Vector3(
+				Mathf.Clamp( Random.Range(-distanceMultiplier, distanceMultiplier), -maxHeadDist, maxHeadDist),
+				Mathf.Clamp( Random.Range(-distanceMultiplier, distanceMultiplier), -maxHeadDist, maxHeadDist),
+				Mathf.Clamp( Random.Range(-distanceMultiplier, distanceMultiplier), -maxHeadDist, maxHeadDist));
+			head.localPosition = neutralHeadPos + randomDir;
 
-        if (angriness > 1.0f)
-        {
-            director.PlayerStared();
-        }
+			if (angriness > 1.0f)
+			{// Game Over
+				director.PlayerStared();
+
+				head.transform.localScale = neutralHeadscale * angriness;
+			}
+			if (angriness > 2.0f)
+			{//POP!
+				head.gameObject.AddComponent<Rigidbody>();
+				head.gameObject.AddComponent<BoxCollider>();
+				head.SetParent(null);
+				headPopSound.Play();
+				head = null;
+			}
+
+		}
+
     }
 
     public void MakeMoreAngry()
